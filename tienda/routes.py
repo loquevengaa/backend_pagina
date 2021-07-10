ecdfe0cff9cee0767fcb0721fc525088ad185f44
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2021-07-10 19:12:40 -0300

    esto deberia andar uwu

--- tienda/routes.py
+++ tienda/routes.py
@@ -1,6 +1,8 @@
 from flask.helpers import flash
 from tienda import app
-from flask import render_template,redirect,url_for,flash,request
+import json
+from flask import render_template,redirect,url_for,flash,request,session, make_response
+
 from tienda.models import Productos,Usuarios
 from tienda.forms import RegisterForm,LoginForm,ComprarProductoForm
 from tienda import db
@@ -11,13 +13,20 @@ login_required es para prohibir que la gente agregue cosas al carrito hasta que 
 y se usa arriba de la funcion de esta manera
 @login_required
 """
+@app.route('/set_cookie')
+def set_cookie():
+    redirect_to_index = redirect('/')
+    response = app.make_response(redirect_to_index )  
+    response.set_cookie('carrito',value=[])
+    return response
+
+
+
+
+
 
 @app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/tienda',methods=['GET','POST'])#aca se muestran lo productos
+@app.route('/home',methods=['GET','POST'])#aca se muestran lo productos
 def tienda_page():
     agregar_al_carrito=ComprarProductoForm()
     if request.method == 'POST':
@@ -29,7 +38,7 @@ def tienda_page():
             pass
 
     items= Productos.query.all()
-    return render_template('tienda.html',items=items,carrito=agregar_al_carrito)
+    return render_template('home.html.html',items=items,carrito=agregar_al_carrito)
 
 @app.route('/registro',methods=['GET','POST'])
 def pagina_registro():
@@ -82,15 +91,75 @@ def logout_page():
 
 
 
+@app.route('/carrito/add/<id>',methods=["get","post"])
+@login_required
+def carrito_add(id):
+	art=Productos.query.get(id)	
+	form=ComprarProductoForm()
+	form.id.data=id
+	if form.validate_on_submit():
+		if art.stock>=int(form.cantidad.data):
+			try:
+				datos = json.loads(request.cookies.get(str(current_user.id)))
+			except:
+				datos = []
+			actualizar= False
+			for dato in datos:
+				if dato["id"]==id:
+					dato["cantidad"]=form.cantidad.data
+					actualizar = True
+			if not actualizar:
+				datos.append({"id":form.id.data,"cantidad":form.cantidad.data})
+			resp = make_response(redirect(url_for('inicio')))
+			resp.set_cookie(str(current_user.id),json.dumps(datos))
+			return resp
+		form.cantidad.errors.append("No hay artículos suficientes.")
+	return render_template("carrito_add.html",form=form,art=art)
 
 
 
 
 
+@app.route('/carrito')
+@login_required
+def carrito():
+	try:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+	except:
+		datos = []
+	articulos=[]
+	cantidades=[]
+	total=0
+	for articulo in datos:
+		articulos.append(Productos.query.get(articulo["id"]))
+		cantidades.append(articulo["cantidad"])
+		total=total+Productos.query.get(articulo["id"]).precio_final()*articulo["cantidad"]
+	articulos=zip(articulos,cantidades)
+	return render_template("carrito.html",articulos=articulos,total=total)
 
 
 
+@app.context_processor
+def contar_carrito():
+	if not current_user.is_authenticated:
+		return {'num_articulos':0}
+	if request.cookies.get(str(current_user.id))==None:
+		return {'num_articulos':0}
+	else:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+		return {'num_articulos':len(datos)}
 
-
-
-
+app.route('/carrito_delete/<id>')
+@login_required
+def carrito_delete(id):
+	try:
+		datos = json.loads(request.cookies.get(str(current_user.id)))
+	except:
+		datos = []
+	new_datos=[]
+	for dato in datos:
+		if dato["id"]!=id:
+			new_datos.append(dato)
+	resp = make_response(redirect(url_for('carrito')))
+	resp.set_cookie(str(current_user.id),json.dumps(new_datos))
+	return resp
