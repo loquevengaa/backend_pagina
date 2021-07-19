d9f84c3ccc10a637ab4d5e8eb4b08bf8397fb59c
blackcoffepls <69984284+blackcoffepls@users.noreply.github.com>
2021-07-19 18:45:09 -0300

    se creo formulario carrito

--- tienda/carrito/routes.py
+++ tienda/carrito/routes.py
@@ -5,33 +5,41 @@ from tienda.models import Productos
 from tienda.carrito.forms import FormCarrito
 
 import json
-
+"""
 @app.route('/carrito/add',methods=["get","post"])
 def carrito_add():
 	form = FormCarrito()
 	art = Productos.query.get(request.form["id"])
-    if art:
-        form.id.data = id
-        form.cantidad.data=cant
-        if form.validate_on_submit():
-            if art.stock >= int(form.cantidad.data):
-                try:
-                    datos = json.loads(request.cookies.get(str(current_user.id)))
-                except:
-                    datos = []
-                actualizar = False
-                for dato in datos:
-                    if dato["id"] == id:
-                        dato["cantidad"] = form.cantidad.data
-                        actualizar = True
-                if not actualizar:
-                    datos.append({"id": form.id.data,
-                                "cantidad": form.cantidad.data})
-                resp = make_response(redirect(url_for('inicio')))
-                resp.set_cookie(str(current_user.id), json.dumps(datos))
-                return resp
-            form.cantidad.errors.append("No hay artículos suficientes.")
-    return render_template('home.html',items=Productos.query.all(),carrito=agregar_al_carrito)
+	if art:
+		form.id.data = id
+		form.cantidad.data=cant
+		if form.validate_on_submit():
+		    if art.stock >= int(form.cantidad.data):
+		        try:
+		            datos = json.loads(request.cookies.get(str(current_user.id)))
+		        except:
+		            datos = []
+		        actualizar = False
+		        for dato in datos:
+		            if dato["id"] == id:
+		                dato["cantidad"] = form.cantidad.data
+		                actualizar = True
+		        if not actualizar:
+		            datos.append({"id": form.id.data,
+		                        "cantidad": form.cantidad.data})
+		        resp = make_response(redirect(url_for('inicio')))
+		        resp.set_cookie(str(current_user.id), json.dumps(datos))
+		        return resp
+		    form.cantidad.errors.append("No hay artículos suficientes.")
+	return render_template('home.html',items=Productos.query.all(),carrito=agregar_al_carrito)"""
+
+@app.route('/carrito/add',methods=["GET","POST"])
+def carrito_add():
+	if request.method=='POST':
+		indice = int(request.form['indice'])
+		cantidad = int(request.form['cantidad'])
+
+	return render_template('home.html',items=Productos.query.all())
 
 
 
--- tienda/routes.py
+++ tienda/routes.py
@@ -1,20 +1,31 @@
 from tienda import app,db
 from tienda.models import Productos
 from tienda.carrito.forms import FormCarrito
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route('/')
-@app.route('/home',methods=['GET','POST'])#aca se muestran lo productos
+@app.route('/home',methods=['POST','GET'])#aca se muestran lo productos
 def tienda_page():
 	agregar_al_carrito=FormCarrito()
 	items= Productos.query.all()
 	return render_template('home.html',items=items,carrito=agregar_al_carrito)
 
 
-@app.route('/categoria/<categoria>',methods=['GET','POST'])#aca se muestran lo productos
+@app.route('/categoria/<categoria>',methods=['POST','GET'])#aca se muestran lo productos
 def categorias(categoria):
 	agregar_al_carrito=FormCarrito()
 	items= Productos.query.filter_by(categoria=categoria)
+
+
+	if request.method=='POST':
+		indice = int(request.form['indice'])
+		cantidad = int(request.form['cantidad'])
+		print(indice)
+		print(cantidad)
+		
+		return redirect(request.referrer)
+
+
 	return render_template('home.html',items=items,carrito=agregar_al_carrito)
 
 
