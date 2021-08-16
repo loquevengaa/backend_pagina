baf88f7df2654c96cb47598b3ddfd6bf78c7b2af
blackcoffepls <69984284+blackcoffepls@users.noreply.github.com>
2021-08-16 17:34:10 -0300

    tarjetas combo en proceso

--- tienda/routes.py
+++ tienda/routes.py
@@ -1,5 +1,5 @@
 from tienda import app
-from tienda.models import Productos,Usuarios
+from tienda.models import Productos,Usuarios,Combos
 import json
 from flask import render_template, request, redirect,make_response,url_for
 from flask_login import current_user,LoginManager
@@ -95,6 +95,57 @@ def categorias(categoria):
 	items= Productos.query.filter_by(categoria=categoria)
 	
 	return render_template('home.html',items=items,articulos=articulos,totalfinal=totalfinal,d=d)
+
+
+@app.route('/categoria/combos',methods=['GET','POST'])#aca se muestran lo productos
+def combos_cliente():
+
+	############################################################# START CARRITO
+	try:
+		datos = json.loads(request.cookies.get("carrito"))
+	except:
+		datos = []
+
+	imagen=[]
+	articulos=[]
+	cantidades=[]
+	total=[]
+	pid=[]
+	stock=[]
+	totalfinal=0
+	for articulo in datos:
+		try:
+			art = Productos.query.get(articulo["id"])
+			
+			pid.append([articulo["id"]])
+			imagen.append(art.imagen)
+			articulos.append(art.nombre)
+			cantidades.append(articulo["cantidad"])
+			aux = art.precioFinal*articulo["cantidad"]
+			stock.append(art.stock)
+			total.append(aux)
+			totalfinal=totalfinal+aux
+		except:
+			pass
+
+	d = len(imagen)	
+	articulos=zip(imagen,articulos,cantidades,total,pid,stock)
+	############################################################# END CARRITO
+	#------------------------------------------------------------------------#
+	############################################################# START COMBOS
+	productos = Productos.query.all()
+	combos= Combos.query.all()
+
+	info = [None]*1000 #aca van los datos del carrito
+	prod = [None]*1000 #aca van los datos del carrito
+	for items in combos:
+		info[items.id] = json.loads(items.datos_combo)
+	for items in  productos:
+		prod[items.id]=items
+
+    ############################################################# END COMBOS
+	
+	return render_template('combos.html',items=combos,articulos=articulos,totalfinal=totalfinal,d=d,info=info,prod=prod)
 
 	#	return redirect(request.referrer)
 
